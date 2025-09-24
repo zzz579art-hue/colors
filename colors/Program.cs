b24cbc0b4314f10657c32b6e64866a4241b1c2de
zzz57 <zzz57@LAPTOP-ABGFVAI3>
2025-09-24 20:08:07 +0500

    Добавьте файлы проекта.

---
+++ colors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string a = "Виктор Никифоров Сиринкс    ";
+            Console.WriteLine(a);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string b = "Бабье лето";
+            Console.WriteLine(b);
+            Console.ForegroundColor = ConsoleColor.Red;
+            string c = "Осень терпкая, как ягода-рябина,\nбрызжет соком золотистых дней.\nВот она, хмельная сердцевина\nлета Бабьего и осени моей.\nИ не потому, что сердце ласки просит,\nнет хмельней осеннего вина.\nОсенью Весна в мою приходит осень,\nчтобы душу высветлить до дна.";
+            Console.WriteLine(c);
+            Console.ResetColor();
+        }
+    }
+}
